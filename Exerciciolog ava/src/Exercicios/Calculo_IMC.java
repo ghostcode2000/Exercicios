0ea6bec1a1c92bff7a22eaf98eb2dc4b95815ae6
Natã Moreira <Nat Moreira@DESKTOP-64D85T9>
2024-09-04 23:33:01 -0500

    Construindo as alternativas

--- Exerciciolog ava/src/Exercicios/Calculo_IMC.java
+++ Exerciciolog ava/src/Exercicios/Calculo_IMC.java
@@ -1,5 +1,40 @@
 package Exercicios;
 
-public class Calculo_IMC {
+import java.util.Scanner;
 
+public class Calculo_IMC {
+	public static void main(String[] args) {
+		
+		//9 - Faça um algoritmo que calcule o IMC (Índice de Massa Corporal) de uma pessoa, 
+		//leia o seu peso e sua altura e imprima na tela sua condição 
+		
+		Scanner scanner = new Scanner(System.in);
+		double  altura;
+		double peso;
+		
+		System.out.println("Informe sua altura : ");
+		altura = scanner.nextDouble();
+		
+		System.out.println("Iforme seu peso: ");
+		peso = scanner.nextDouble();
+		
+		double IMC = peso / (altura*altura);
+		
+		System.out.println("Seu IMC foi : "+IMC);
+		
+		if (IMC < 18.5) {
+			System.out.println("Vc está Abaixo do peso.");			
+		}
+			if (IMC > 18.5 && IMC <=24.9 ) {
+				System.out.println("Vc está no peso ideal. ");				
+			}
+				if (IMC >= 25.0 && IMC <= 29.9) {
+					System.out.println("Vc está levemente acima do peso");
+				}
+					
+		
+		
+		
+	
+	}
 }
