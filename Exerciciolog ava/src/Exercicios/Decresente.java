db12ba783fadff9cf57ad31763e77de03186ba5a
Natã Moreira <Nat Moreira@DESKTOP-64D85T9>
2024-09-04 22:53:45 -0500

    Ordem decrecente

--- Exerciciolog ava/src/Exercicios/Decresente.java
+++ Exerciciolog ava/src/Exercicios/Decresente.java
@@ -20,21 +20,41 @@ public class Decresente {
      System.out.println("Digite o terceiro valor: ");
      valor3 = scanner.nextInt();	
      
-     int maior,meio,menor;
-     
-     if ( valor1 > valor2 && valor1 > valor3 ) {
-    	maior = valor1 ;
-     }if(valor2 > valor3) {
-    	 meio = valor2;
-    	 menor = valor3;
-     }else {
-    	 meio = valor3;
-    	 menor = valor2;
+     int maior, meio, menor;
+
+     if (valor1 > valor2 && valor1 > valor3) {
+         maior = valor1;
+         if (valor2 > valor3) {
+             meio = valor2;
+             menor = valor3;
+         } else { 
+             meio = valor3;
+             menor = valor2;
+         }
+     } else if (valor2 > valor1 && valor2 > valor3) {
+         maior = valor2;
+         if (valor1 > valor3) {
+             meio = valor1;
+             menor = valor3;
+         } else {
+             meio = valor3;
+             menor = valor1;
+         }
+     } else {
+         maior = valor3;
+         if (valor1 > valor2) {
+             meio = valor1;
+             menor = valor2;
+         } else {
+             meio = valor2;
+             menor = valor1;
+         }
      }
-       if (valor2 > valor1 && valor2 > valor3) {
-    	   maior = valor2;
-       }if
-       
+
+     // Exibição dos valores em ordem decrescente
+     System.out.println("Valores em ordem decrescente: " + maior + ", " + meio + ", " + menor);
+   scanner.close();
+       }
 	}
 
-}
+
